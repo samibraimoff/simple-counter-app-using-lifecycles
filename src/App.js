d09e9f4abe6952e8f104e03d24b649089fdbff79
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-03-15 14:43:34 -0400

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    counter: 0,
+    isCounting: false,
+  };
+
+  componentDidMount() {
+    const userCount = localStorage.getItem('timer');
+    if (userCount) {
+      this.setState({ counter: +userCount });
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('timer', this.state.counter);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.counterId);
+  }
+
+  handleStart = () => {
+    this.setState({ isCounting: true });
+
+    this.counterId = setInterval(() => {
+      this.setState({ counter: this.state.counter + 1 });
+    }, 1000);
+  };
+
+  handleStop = () => {
+    this.setState({ isCounting: false });
+    clearInterval(this.counterId);
+  };
+
+  handleReset = () => {
+    this.setState({ counter: 0, isCounting: false });
+    clearInterval(this.counterId);
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Timer</h1>
+        <h2>{this.state.counter}</h2>
+        <div>
+          {!this.state.isCounting ? (
+            <button onClick={this.handleStart}>start</button>
+          ) : (
+            <button onClick={this.handleStop}>end</button>
+          )}
+          <button onClick={this.handleReset}>reset</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
